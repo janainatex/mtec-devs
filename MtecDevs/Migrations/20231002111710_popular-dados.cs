fe615999941e0c366de026adbb758bf44810f60a
janainatex <janainajapa05@gmail.com>
2023-10-02 08:22:38 -0300

    concluido a criacao dos dados

---
+++ MtecDevs/Migrations/20231002111710_popular-dados.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MtecDevs.Migrations
+{
+    public partial class populardados : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
