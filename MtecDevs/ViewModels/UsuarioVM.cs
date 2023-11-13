a21ab7dd852bf49a7f98a130416ca521063228f8
janainatex <janainajapa05@gmail.com>
2023-11-13 08:57:23 -0300

    criação do serviço de acesso ao usuário logado

---
+++ MtecDevs/ViewModels/UsuarioVM.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MtecDevs.ViewModels
+{
+    public class UsuarioVM
+    {
+        
+    }
+}
